1bdaaee4d8a20447ee1f8f8d176e497a88cf59fd
Mark Boyd <mark.boyd@gsa.gov>
2022-08-22 15:50:45 -0400

    fix script

--- jobs/elastalert2/templates/bin/elastalert2.sh
+++ jobs/elastalert2/templates/bin/elastalert2.sh
@@ -6,7 +6,7 @@ set -x
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
 
-export PATH=/var/vcap/packages/curator/bin:$PATH
+export PATH=/var/vcap/packages/elastalert2/bin:$PATH
 export CONFIG_DIR=/var/vcap/jobs/elastalert2/config
 export PYTHONPATH=/var/vcap/packages/elastalert2/lib/python3.7/site-packages/
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/var/vcap/packages/python3/lib
