e06ecc70f543a6979958d93ee31509e222c66ec0
Mark Boyd <mark.boyd@gsa.gov>
2022-08-22 16:04:20 -0400

    add stub of config file

--- jobs/elastalert2/templates/bin/elastalert2.sh
+++ jobs/elastalert2/templates/bin/elastalert2.sh
@@ -11,4 +11,4 @@ export CONFIG_DIR=/var/vcap/jobs/elastalert2/config
 export PYTHONPATH=/var/vcap/packages/elastalert2/lib/python3.7/site-packages/
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/var/vcap/packages/python3/lib
 
-elastalert2 --config $CONFIG_DIR/config.yaml
+python elastalert/elastalert.py --config $CONFIG_DIR/config.yml
