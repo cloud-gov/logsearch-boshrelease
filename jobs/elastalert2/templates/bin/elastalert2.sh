c1c3b7f024a307a8bccc4e7c6c689e62ecf14a10
Mark Boyd <mark.boyd@gsa.gov>
2022-08-22 15:44:09 -0400

    update stub package

---
+++ jobs/elastalert2/templates/bin/elastalert2.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
+
+export PATH=/var/vcap/packages/curator/bin:$PATH
+export CONFIG_DIR=/var/vcap/jobs/elastalert2/config
+export PYTHONPATH=/var/vcap/packages/elastalert2/lib/python3.7/site-packages/
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/var/vcap/packages/python3/lib
+
+elastalert2 --config $CONFIG_DIR/config.yaml
